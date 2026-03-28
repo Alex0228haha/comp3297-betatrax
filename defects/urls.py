4e7f74d283c4105624936d10ac6ea63079c91ec3
SHAWNMICHAELNGWENO <shawnmichaelngweno@gmail.com>
2026-03-28 13:29:35 +0800

    Added defect management for developers. Developers can view a list of open defects, assign defects, mark as fixed or cannot reproduce.

---
+++ defects/urls.py
@@ -0,0 +1,14 @@
+"""
+URL configuration for defects app.
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Developer endpoints
+    path('api/developer/defects/open/', views.list_open_defects, name='list_open_defects'),
+    path('api/developer/defects/<int:defect_id>/', views.view_defect_detail, name='view_defect_detail'),
+    path('api/developer/defects/<int:defect_id>/take-responsibility/', views.take_responsibility, name='take_responsibility'),
+    path('api/developer/defects/<int:defect_id>/mark-as-fixed/', views.mark_as_fixed, name='mark_as_fixed'),
+    path('api/developer/defects/<int:defect_id>/mark-as-cannot-reproduce/', views.mark_as_cannot_reproduce, name='mark_as_cannot_reproduce'),
+]
